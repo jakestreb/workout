0e419e03080203b47985f74d6d93806513918093
Jake Streb <jake@getgrist.com>
2021-09-19 22:27:52 -0400

    remove log

--- src/generators/BasicGenerator.ts
+++ src/generators/BasicGenerator.ts
@@ -18,7 +18,6 @@ export default class BasicGenerator {
 	}
 
 	public* generate() {
-		console.warn('called generate');
 		const exercisePicker = new ExercisePicker(this.target);
 
 		for (const exercises of exercisePicker.pick()) {
