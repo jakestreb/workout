7e19ffa85d3cae438a43a8377a0d1582b1864dbe
Jake Streb <jake@getgrist.com>
2021-11-10 23:53:27 -0500

    trying to make it build

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,76 +1,72 @@
-declare global {
-	interface Target {
-		muscles: string[];
-		intensity: number;
-		timeMinutes: number;
-	}
-
-	interface TargetRecord {
-		name: string;
-		phases: TargetRecordPhase[];
-	}
-
-	interface TargetRecordPhase {
-		muscles: string[];
-		weight: number;
-	}
-
-	interface GeneratorProgress {
-		generated: number,
-		filtered: number,
-		isDone: boolean,
-	}
-
-	interface RecordBasics {
-	  exercise: string,
-	  sets: number,
-	  reps: number,
-	  weight: number,
-	  completed: boolean,
-	  created_at: string
-	}
-
-	interface DBRecord extends RecordBasics {
-	  user_id: number,
-	  workout_id: string
-	}
-
-	interface DBUser {
-	  name: string,
-	  gender: 'male'|'female'|'other',
-	  experience: 'beginner'|'intermediate'|'advanced',
-	  primary_focus: 'strength'|'endurance'
-	}
-
-	interface JSONExercise {
-	    name: string,
-	    weight: number,
-	    activations: JSONActivation[],
-	    secondsPerRep: number,
-	    sets: number[]
-	    reps: number[]
-	    skills: {
-	    	endurance: number,
-	     	strength: number,
-	    },
-	    weightStandards: {
-	    	male: number,
-	    	female: number
-	    }
-	    supersetGroups: string[],
-	    isBodyweightExercise: boolean
-	}
-
-	interface JSONActivation {
-        muscle: string,
-        intensityPerRep: number
-	}
-
-	interface JSONMuscle {
-		name: string;
-		defaultWeight?: number;
-		children?: JSONMuscle[];
-	}
+declare interface Target {
+	muscles: string[];
+	intensity: number;
+	timeMinutes: number;
 }
 
-export {};
+declare interface TargetRecord {
+	name: string;
+	phases: TargetRecordPhase[];
+}
+
+declare interface TargetRecordPhase {
+	muscles: string[];
+	weight: number;
+}
+
+declare interface GeneratorProgress {
+	generated: number,
+	filtered: number,
+	isDone: boolean,
+}
+
+declare interface RecordBasics {
+  exercise: string,
+  sets: number,
+  reps: number,
+  weight: number,
+  completed: boolean,
+  created_at: string
+}
+
+declare interface DBRecord extends RecordBasics {
+  user_id: number,
+  workout_id: string
+}
+
+declare interface DBUser {
+  name: string,
+  gender: 'male'|'female'|'other',
+  experience: 'beginner'|'intermediate'|'advanced',
+  primary_focus: 'strength'|'endurance'
+}
+
+declare interface JSONExercise {
+    name: string,
+    weight: number,
+    activations: JSONActivation[],
+    secondsPerRep: number,
+    sets: number[]
+    reps: number[]
+    skills: {
+    	endurance: number,
+     	strength: number,
+    },
+    weightStandards: {
+    	male: number,
+    	female: number
+    }
+    supersetGroups: string[],
+    isBodyweightExercise: boolean
+}
+
+declare interface JSONActivation {
+    muscle: string,
+    intensityPerRep: number
+}
+
+declare interface JSONMuscle {
+	name: string;
+	defaultWeight?: number;
+	children?: JSONMuscle[];
+}
